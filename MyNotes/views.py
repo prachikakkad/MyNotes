26154485b892593e615e33aa5daa4dcc6d089fc7
Prachi Kakkad <87642173+prachikakkad@users.noreply.github.com>
2022-08-28 04:13:52 +0530

    Update views.py

--- MyNotes/views.py
+++ MyNotes/views.py
@@ -19,3 +19,14 @@ def C(request):
     response['Content-Disposition'] = "Attachment ; filename = %s" % filename
     return response
 
+def HTML(request):
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    filename = 'My HTML Notes.pdf'
+    filepath = base_dir + '/Templates/' + filename
+    thefile = filepath
+    filename = os.path.basename(thefile)
+    chunk_size = 8192
+    response = StreamingHttpResponse(FileWrapper(open(thefile , 'rb') , chunk_size),content_type=mimetypes.guess_type(thefile)[0])
+    response['Content-Length'] = os.path.getsize(thefile)
+    response['Content-Disposition'] = "Attachment ; filename = %s" % filename
+    return response
